8ce1be3416fae38aa280e0ee998671f73c677f1f
Dan Gillespie <daniel.gillespie@pluto.tv>
2020-03-01 12:41:03 -0800

    added restart

--- bin/start.sh
+++ bin/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 source /etc/environment
-docker run -v $OVPN_CONFIG_PATH:/etc/openvpn -d -p 443:1194/tcp --cap-add=NET_ADMIN kylemanna/openvpn
+docker run -v $OVPN_CONFIG_PATH:/etc/openvpn -d -p 443:1194/tcp --cap-add=NET_ADMIN --restart=always kylemanna/openvpn
