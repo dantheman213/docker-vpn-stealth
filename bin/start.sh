35670110a89fc0c0aa95a655d4e1fc6d0279a4b7
Dan Gillespie <daniel.gillespie@pluto.tv>
2020-03-01 14:18:56 -0800

    port fix

--- bin/start.sh
+++ bin/start.sh
@@ -6,4 +6,4 @@ echo "Starting VPN..."
 docker run -v $OVPN_CONFIG_PATH:/etc/openvpn -d -p 443:1194/tcp --cap-add=NET_ADMIN --restart=always kylemanna/openvpn
 
 echo "Configuring nginx subterfuge proxy..."
-docker run -d -p 8443:80/tcp --restart=always nginx:latest
+docker run -d -p 127.0.0.1:8443:80/tcp --restart=always nginx:latest
